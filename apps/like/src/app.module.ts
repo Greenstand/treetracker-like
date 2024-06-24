ba49cef40c25ad84f9f74a23caaa719cd5592019
Guning Shen <guningshen@gmail.com>
2024-06-24 22:29:42 +0800

    Feat: add prisma seed

--- apps/like/src/app.module.ts
+++ apps/like/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ObjectModule } from './object/object.module';
 import { TypeModule } from './type/type.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ObjectModule,
     TypeModule,
-    UserModule
+    UserModule,
+    PrismaModule
   ],
   controllers: [],
   providers: [],
