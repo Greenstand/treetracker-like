0179a49aa0460eb3f1d7074069a804f7518ef703
Mohmn <naqashmohmin1@gmail.com>
2024-07-13 02:58:48 +0530

    feat: migrate from nx to yarn

--- apps/like/src/user/user.service.ts
+++ apps/like/src/user/user.service.ts
@@ -1,39 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from '@like-button-sample/shared';
+import { CreateUserDto } from 'libs/validation';
 import { prisma } from '../../prisma/prisma';
 
 @Injectable()
 export class UserService {
   constructor() {}
 
-  async createUser(createUserDto: CreateUserDto) {
-    const existedUser = await prisma.user.findFirst({
-      where: { id: createUserDto.id }
-    });
-    if (existedUser != null) {
-      console.log(`user ${existedUser.name} already exists`);
-      return null;
-    }
-    console.log(`saving user ${createUserDto.name}...`);
-    return await prisma.user.create({
-      data: {
-        id: createUserDto.id,
-        username: createUserDto.username,
-        name: createUserDto.name,
-      },
-    });
-  }
+  // async createUser(createUserDto: CreateUserDto) {
+  //   const existedUser = await prisma.user.findFirst({
+  //     where: { id: createUserDto.id }
+  //   });
+  //   if (existedUser != null) {
+  //     console.log(`user ${existedUser.name} already exists`);
+  //     return null;
+  //   }
+  //   console.log(`saving user ${createUserDto.name}...`);
+  //   return await prisma.user.create({
+  //     data: {
+  //       id: createUserDto.id,
+  //       username: createUserDto.username,
+  //       name: createUserDto.name,
+  //     },
+  //   });
+  // }
 
-  async getUserById(userId: string) {
-    return await prisma.user.findFirst({
-      where: { id: userId }
-    });
-  }
+  // async getUserById(userId: string) {
+  //   return await prisma.user.findFirst({
+  //     where: { id: userId }
+  //   });
+  // }
 
-  async getPostsByUser (username: string) {
-    return await prisma.user.findFirst({
-      where: { username: username },
-      include: { createdPosts: true }
-    });
-  }
+  // async getPostsByUser (username: string) {
+  //   return await prisma.user.findFirst({
+  //     where: { username: username },
+  //     include: { createdPosts: true }
+  //   });
+  // }
 }
